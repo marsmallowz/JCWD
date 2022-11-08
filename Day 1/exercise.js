b8d819a8c7b8059ea492d1f843c92b63605491b5
marsmallowz <alsandymaulana15@email.com>
2022-11-08 15:22:56 +0700

    Add math.abs on find difference between date and change var to let

--- Day 1/exercise.js
+++ Day 1/exercise.js
@@ -17,7 +17,7 @@ console.log(diameter);
 console.log(circumference);
 console.log(area);
 
-// findAnglesofTriangle
+// findAnglesOfTriangle
 let angle1Circle = 20;
 let angle2Circle = 90;
 angles = 180 - angle1Circle + angle2Circle;
@@ -26,14 +26,15 @@ console.log(angles);
 //differenceBetweenDatesInDays
 date1 = new Date("11/10/2022");
 date2 = new Date("12/13/2022");
-differenceInDays = (date2.getTime() - date1.getTime()) / (1000 * 3600 * 24);
+differenceInDays =
+  Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24);
 console.log(differenceInDays);
 
 // convertDaysToYearsMonthsDays
 let numberOfDays = 400;
-var years = Math.floor(numberOfDays / 365);
-var months = Math.floor((numberOfDays % 365) / 30);
-var days = Math.floor((numberOfDays % 365) % 30);
+let years = Math.floor(numberOfDays / 365);
+let months = Math.floor((numberOfDays % 365) / 30);
+let days = Math.floor((numberOfDays % 365) % 30);
 console.log(`${years} Year, ${months} Month, ${days} days`);
 console.log(
   `${years} Year: ${years * 365} days, ${months} Months : ${months * 30} days`
